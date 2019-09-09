efc700513c348c7fee36974c29681e54bb5bf60d
tungnt112 <tungnt112@cyberspace.vn>
2019-09-09 19:04:49 +0700

    update base

--- routes/Brandname/SegmentWarningConfigHistory.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['prefix' => '/brandname-segment-warning-config-history', 'as' => 'brandname-segment-warning-config-history.'], function () {
-    Route::post('listing', 'BrandnameSegmentWarningConfigHistoryController@listing')->name('listing');
-});
